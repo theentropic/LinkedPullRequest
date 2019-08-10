c815edb2032a38e1d8a25452049cd5b9247e16d6
theentropic <edabramson@gmail.com>
2019-08-10 14:34:43 -0400

    Remove dist folder and start work

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+import * as vsts from "azure-devops-node-api";
 import express from "express";
 
 const app = express();
 const port = 3000;
+
+const collectionURL = process.env.COLLECTIONURL;
+const token = process.env.TOKEN;
+
+const authHandler = vsts.getPersonalAccessTokenHandler(token);
+const connection = new vsts.WebApi(collectionURL, authHandler);
+
+// tslint:disable-next-line:no-console
+const vstsGit = connection.getGitApi().then((success) => { console.log(success); }, (error) => { console.log(error); });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
